4c613d4d746acec731f8125f22e007c448faf310
Artem Jasan <artem.jasan@applifting.cz>
2023-04-17 11:11:30 +0200

    Update backend config file

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,5 +1,5 @@
 import os
-from pydantic import BaseSettings
+from pydantic import BaseSettings, AnyHttpUrl, validator
 
 
 class PostgresSettings(BaseSettings):
@@ -20,10 +20,21 @@ class Settings(BaseSettings):
     PREFIX_API: str = "/api"
     # Database settings
     POSTGRES: PostgresSettings = PostgresSettings()
+    # Other settings
+    BACKEND_CORS_ORIGINS: list[AnyHttpUrl] = ["http://localhost:3000", "http://0.0.0.0:3000", "http://127.0.0.1:3000"]
 
     def get_db_url(self) -> str:
         pg = self.POSTGRES
         return f"postgresql+asyncpg://{pg.USER}:{pg.PASSWORD}@{pg.HOST}:{pg.PORT}/{pg.DB}"
+
+    @validator("BACKEND_CORS_ORIGINS", pre=True)
+    def assemble_cors_origins(cls, v: str | list[str]) -> list[str]:
+        if isinstance(v, str) and not v.startswith("["):
+            return [i.strip() for i in v.split(",")]
+        elif isinstance(v, (list, tuple)):
+            return [str(x).strip() for x in v]
+        raise ValueError(v)
+
 
 
 settings = Settings()
