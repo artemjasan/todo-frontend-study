104293e7a867660a7a89813af571a2c7b60bf2ac
no-need-to-be-anonymous <oantonjak@gmail.com>
2023-04-29 11:44:57 +0200

    chore: clsx example

--- frontend/src/components/buttons/BaseButton.tsx
+++ frontend/src/components/buttons/BaseButton.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import BaseButtonProps from './buttonInterfaces';
 import '../../index.css';
+import clsx from 'clsx';
 
 export const BaseButton: React.FC<BaseButtonProps> = ({ onClick, className, label }) => {
   return (
-    <button onClick={onClick} className={`base-button ${className}`}>
+    <button onClick={onClick} className={clsx(className, 'base-button')}>
       {label}
     </button>
   );
